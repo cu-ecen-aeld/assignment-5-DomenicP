a124f93785f04e8be18f9375a79de94f9090dc33
Domenic Rodriguez <domenicpaul@me.com>
2024-09-29 15:11:44 -0400

    Add LDD kernel module package

---
+++ base_external/package/ldd/ldd.mk
@@ -0,0 +1,19 @@
+
+##############################################################
+#
+# LDD
+#
+##############################################################
+
+LDD_VERSION = '2b4d19c947db764fa440644e6530054960584e67'
+# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
+# with ssh keys and the automated build/test system.
+# Your site should start with git@github.com:
+LDD_SITE = 'git@github.com:cu-ecen-aeld/assignment-7-DomenicP.git'
+LDD_SITE_METHOD = git
+LDD_GIT_SUBMODULES = YES
+
+LDD_MODULE_SUBDIRS = misc-modules scull
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
